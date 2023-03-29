380aa18a79d38ec2ea2d5305b82a255e6d3cd69d
SergioSoto19 <ser192000gio@gmail.com>
2023-03-29 07:51:54 -0500

    medio froan ,falta servi de inscrip
    en el inscripcion solo se imprime por consola no se envia , el hom Hom de vienvenido no se centra

--- src/app/inscripciones/agregar-inscripcion/agregar-inscripcion.component.ts
+++ src/app/inscripciones/agregar-inscripcion/agregar-inscripcion.component.ts
@@ -10,13 +10,13 @@ export class AgregarInscripcionComponent implements OnInit {
   selectedSubject: any;
   selectedDate: string;
   
-  //currentDate: string = new Date().toISOString().slice(0, 10)
   currentDate : string= new Date().toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-').split("-").reverse().join("-");
 
   constructor() {
     this.selectedDate = this.currentDate;
   }
 
+  //se envia a consola pero deberia llamar a servicio post de inscripciones
   onButtonClick() {
     console.log(this.estudiante.codigo,this.selectedSubject.codigo,this.selectedDate);
   }

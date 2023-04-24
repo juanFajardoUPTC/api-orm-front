a1f4a788569a4f86ff20f775302b38cd47bc4c91
SergioSoto19 <ser192000gio@gmail.com>
2023-04-24 14:46:05 -0500

    fondo  y cuadri semi actualiz

--- src/app/materias/agregar-materia/agregar-materia.component.ts
+++ src/app/materias/agregar-materia/agregar-materia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Materia } from '../materia.model';
 import { NgForm } from '@angular/forms';
 import { ServicioMateriasService } from 'src/app/services/servicio-materias.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-agregar-materia',
@@ -13,7 +14,8 @@ export class AgregarMateriaComponent implements OnInit {
   
   model = new Materia(810, "", 0, "");
 
-  constructor(private servicioMaterias: ServicioMateriasService) { }
+  constructor(private servicioMaterias: ServicioMateriasService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -24,11 +26,11 @@ export class AgregarMateriaComponent implements OnInit {
   addSubject() {
     this.servicioMaterias.postRequest(this.model).subscribe(
       (respuesta: any) => {
-        // Manejar la respuesta exitosa aquí
+        this.toastr.success('La Asignatura se Registro corrctamente','Registro Correcto');
         console.log(respuesta);
       },
       error => {
-        // Manejar el error aquí
+        this.toastr.error('Error al Registrar Asignatuta','Error de Registro');
         console.error(error);
       }
     );
